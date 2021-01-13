bcfc81483d001cff361cfd588094990ae7eb860f
polunzh <polunzh@gmail.com>
2021-01-13 08:38:55 +0800

    no longer publish to npm scripts and fix deploy to abtnode bug

--- tools/publish.sh
+++ tools/publish.sh
@@ -12,14 +12,13 @@ sudo npm install -g @abtnode/cli
 
 echo "publishing blocklet development guide blocklet..."
 npm run bundle
-npm publish .blocklet/bundle --access=public
 
 # deploy to remote ABT Node
 set +e
 NAME=$(cat package.json | grep name |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 VERSION=$(cat package.json | grep version |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 if [ "${ALIYUN_ENDPOINT}" != "" ]; then
-  blocklet deploy .blocklet/bundle --endpoint ${ALIYUN_ENDPOINT} --access-key ${ALIYUN_ACCESS_KEY} --access-secret ${ALIYUN_ACCESS_SECRET} --skip-hooks
+  blocklet deploy .blocklet/bundle --endpoint ${ALIYUN_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
     echo "deploy to ${ALIYUN_ENDPOINT} success"
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_ENDPOINT}\"}" ${SLACK_WEBHOOK}
@@ -29,7 +28,7 @@ if [ "${ALIYUN_ENDPOINT}" != "" ]; then
   fi
 fi
 if [ "${AWS_ENDPOINT}" != "" ]; then
-  blocklet deploy .blocklet/bundle --endpoint ${AWS_ENDPOINT} --access-key ${AWS_ACCESS_KEY} --access-secret ${AWS_ACCESS_SECRET} --skip-hooks
+  blocklet deploy .blocklet/bundle --endpoint ${AWS_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
     echo "deploy to ${AWS_ENDPOINT} success"
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_ENDPOINT}\"}" ${SLACK_WEBHOOK}
